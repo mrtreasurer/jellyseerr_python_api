050d5c03d1f3ef2cfb423ecfe87db4876e7b9c38
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2023-03-18 18:53:17 +0100

    fix loading .env file, add local

--- source/settings.py
+++ source/settings.py
@@ -1,8 +1,15 @@
 import os
+import pathlib
 
 from dotenv import load_dotenv
 
+root = pathlib.Path(__file__).parent.parent
+print(root)
+try:
+    load_dotenv(root / ".local.env")
 
-load_dotenv()
+except FileNotFoundError:
+    load_dotenv(root / ".env")
+
 
 JELLYSEERR_URL = f"{os.getenv('JELLYSEERR_URL')}:{os.getenv('JELLYSEERR_PORT')}/api/v1/"
