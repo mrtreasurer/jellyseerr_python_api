8ca5e906c3c81732a282f5dc7681154137885802
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2023-03-19 17:20:35 +0100

    Revert "try to get tests running"
    This reverts commit b5756dd9b16b7dd0928df6fa1ec971b341cc51fa.

--- tests/test_auth_new_docker.py
+++ tests/test_auth_new_docker.py
@@ -7,11 +7,14 @@ from source.settings import JELLYSEERR_URL
 
 
 class TestAuth(unittest.TestCase):
+    # def test_api(self):
+    #     response = requests.get("http://localhost:5055/api/v1", headers={"X-Api-Key": ""})
+    #     self.assertEqual(response.status_code, HTTPStatus.OK)
+
     def test_login(self):
         payload = {
             "username": "admin",
-            "password": "password",
-            "hostname": "http://jellyfin"
+            "password": "password"
         }
         response1 = requests.post(JELLYSEERR_URL + "auth/jellyfin", json=payload)
         self.assertEqual(response1.status_code, HTTPStatus.OK)
@@ -19,7 +22,6 @@ class TestAuth(unittest.TestCase):
         response2 = requests.get("http://jellysseerr:5055/api/v1", headers={"X-Api-Key": response1.json()["jellyfinAuthToken"]})
         print(response2.json())
         self.assertEqual(response2.status_code, HTTPStatus.OK)
-
 
 if __name__ == "__main__":
     unittest.main()
