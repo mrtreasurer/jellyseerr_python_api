b5756dd9b16b7dd0928df6fa1ec971b341cc51fa
Bert Van den Abbeele <bert_vandenabbeele@hotmail.com>
2023-03-19 17:20:03 +0100

    try to get tests running

--- tests/test_auth_new_docker.py
+++ tests/test_auth_new_docker.py
@@ -7,14 +7,11 @@ from source.settings import JELLYSEERR_URL
 
 
 class TestAuth(unittest.TestCase):
-    # def test_api(self):
-    #     response = requests.get("http://localhost:5055/api/v1", headers={"X-Api-Key": ""})
-    #     self.assertEqual(response.status_code, HTTPStatus.OK)
-
     def test_login(self):
         payload = {
             "username": "admin",
-            "password": "password"
+            "password": "password",
+            "hostname": "http://jellyfin"
         }
         response1 = requests.post(JELLYSEERR_URL + "auth/jellyfin", json=payload)
         self.assertEqual(response1.status_code, HTTPStatus.OK)
@@ -22,6 +19,7 @@ class TestAuth(unittest.TestCase):
         response2 = requests.get("http://jellysseerr:5055/api/v1", headers={"X-Api-Key": response1.json()["jellyfinAuthToken"]})
         print(response2.json())
         self.assertEqual(response2.status_code, HTTPStatus.OK)
+
 
 if __name__ == "__main__":
     unittest.main()
